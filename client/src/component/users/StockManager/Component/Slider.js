97d6b750111424fb1067f2948cfb8b36f6453a74
pasindu-nawodya <swpasindu921@gmail.com>
2020-04-13 13:41:47 +0530

    Footer edit

--- client/src/component/users/StockManager/Component/Slider.js
+++ client/src/component/users/StockManager/Component/Slider.js
@@ -8,22 +8,22 @@ export default class Slider extends Component {
       var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
         return (
-        <div>
-          <main role="main">
-            <section class="jumbotron text-center">
-              <div class="container">
-                <h1 style={{fontSize:"70px"}}><b>Dashboard</b></h1>
-                <h4>Welcome Pasindu </h4><br />              
-                <p class="lead text-muted"><b>Current Date: {date} </b></p>                
-                <p>
-                  <a href="#" class="btn btn-primary my-2">Production List</a>
-                  <span>&nbsp;&nbsp;</span>
-                  <a href="#" class="btn btn-secondary my-2">Item Operation</a>
-                </p>
-              </div>            
-            </section>
-          </main>
-        </div>
+            <div>
+              <main role="main">
+                <section class="jumbotron text-center">
+                  <div class="container">
+                    <h1 style={{fontSize:"60px"}}><b>Stock Manager Dashboard</b></h1>
+                    <h4>Welcome Pasindu </h4><br />              
+                    <p class="lead text-muted"><b>Current Date: {date} </b></p>                
+                    <p>
+                      <a href="#" class="btn btn-primary my-2">Production List</a>
+                      <span>&nbsp;&nbsp;</span>
+                      <a href="#" class="btn btn-secondary my-2">Item Operation</a>
+                    </p>
+                  </div>            
+                </section>
+              </main>
+            </div>
         )
     }
 }
--- client/src/component/users/StockManager/Dashboard.js
+++ client/src/component/users/StockManager/Dashboard.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import NavBar from './Component/NavBar';
 import Slider from './Component/Slider';
+import Cards from './Component/Cards';
+import Footer from './Component/Footer';
 
 export default class Dashboard extends Component {
     render() {
-        return (
+
+        const value=[{headers :"Available Stock",values : "200"},
+                     {headers :"Total Stock Value",values : "200"},
+                     {headers :"Catogary Count",values : "200"}];
+
+        return (            
         
             <div>    
                 <NavBar />                   
                 <Slider />
+                <div class="container">
+                    <Cards body={value} />
+                </div>
+                <Footer />
+
             </div>
         )
     }
