32e976c3e098655cf059c614619a401ce8e2a779
pasindu-nawodya <swpasindu921@gmail.com>
2020-05-17 12:36:42 +0530

    show product detail list

--- client/src/component/StockManager/Component/ProductTable.js
+++ client/src/component/StockManager/Component/ProductTable.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react'
 
 export default class ProductTable extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            items:[],
+            isLoaded:false,
+        }
+    }
+
+    componentDidMount(){
+
+        fetch('http://localhost:4000/product')
+        .then(res=>res.json())
+        .then(json=>{
+            this.setState({
+                isLoaded:true,
+                items:json,
+                count:0,
+            })
+        });
+    }
+
     render() {
+
+        var {isLoaded,items,count} = this.state;
+
+        if(!isLoaded){
+            return (<center>                    
+                        <div className="spinner-grow" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                        <h5> <b><br />Wait ! Data Is Loading ... </b></h5>
+                    </center>
+            )
+        }
+
         return (
             <div className="container">
 
                 <table className="table" style={{width:"100%"}}>
                     <thead>
                         <tr className="table-active">
-                            <th scope="col">Code</th>
+                            <th scope="col">No</th>
                             <th scope="col">Product Name</th>
                             <th scope="col">Quantity</th>
                             <th scope="col">Details</th>
@@ -17,14 +52,16 @@ export default class ProductTable extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Otto</td>
-                            <td>@mdo</td>
+                    {items.map(item=>(
+                        <tr key={item._id}>
+                            <th scope="row">{++count}</th>
+                            <td>{item.pname}</td>
+                            <td>{item.pqty}</td>
                             <td><a href="#"><button className="btn btn-outline-primary btn-sm">Details</button></a></td>
                             <td><a href="#"><button className="btn btn-outline-secondary btn-sm">Update</button></a></td>
                             <td><a href="#"><button className="btn btn-outline-danger btn-sm">Delete</button></a></td>
                         </tr>
+                    ))}                        
                     </tbody>
                 </table>
                 
