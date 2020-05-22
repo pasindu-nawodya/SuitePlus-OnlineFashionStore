abb98fcc1c03c0ffffad2d4b40693b254a6b60f9
pasindu-nawodya <swpasindu921@gmail.com>
2020-05-23 00:15:03 +0530

    Home Page Edit

---
+++ client/src/component/User/HomePage/DiscountCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class DiscountCard extends Component {
+    render() {
+        return (
+            <div>
+                <center>
+                <div className="card text-center w-75">
+                    <div class="card-header">
+                        <h3><b>Discount Available Items</b></h3>
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">We offer some discount end of the season</h5>
+                        <p className="card-text">To get that Benefits Click the Discount Button Below</p>
+                        <button className="btn btn-danger">Discounts</button>
+                    </div>
+                    <div className="card-footer text-muted">
+                        Get the benefits
+                    </div>
+                </div>
+                </center>
+            </div>
+        )
+    }
+}
--- client/src/component/User/HomePage/Home.js
+++ client/src/component/User/HomePage/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import HomeBanner from './HomeBanner';
+import DiscountCard from './DiscountCard';
+import Selected from './HomepageProducts';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <HomeBanner />
+                <br />
+                <DiscountCard/>
+                <br />
+                <Selected />
             </div>
         )
     }
