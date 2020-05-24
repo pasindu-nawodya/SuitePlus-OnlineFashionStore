8f5e3ac396c3ccbe1ffbd2635573b54225e91e93
pasindu-nawodya <swpasindu921@gmail.com>
2020-05-24 18:35:23 +0530

    final

--- client/src/component/User/HomePage/HomepageProducts.js
+++ client/src/component/User/HomePage/HomepageProducts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import image from './img/shirt.jpg';
 import { faCartPlus,faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Alert from 'react-bootstrap/Alert';
--- client/src/component/User/ProductPage/ProductDetails.js
+++ client/src/component/User/ProductPage/ProductDetails.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import image from './img/shirt.jpg';
 import Selected from './SelectedProduct';
 import { faCartPlus,faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
